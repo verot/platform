5a7c4f6e6645709dc3292ec69fe0d17962db5ddc
Salador <Salador2005@yandex.ru>
2020-09-29 14:50:13 +0000

    Fix toast

--- resources/views/partials/toast.blade.php
+++ resources/views/partials/toast.blade.php
@@ -7,15 +7,15 @@
              data-delay="5000"
              data-autohide="true">
             <div class="toast-body p-3 bg-white rounded shadow-sm">
-                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" title="Close" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
                 <p class="mb-0">
                     <span class="text-{type}">
                         <x-orchid-icon path="circle" class="mr-2"/>
                     </span>
                     {message}
                 </p>
+                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" title="Close" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
             </div>
         </div>
     </template>
@@ -25,9 +25,6 @@
         <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="{{ session(\Orchid\Alert\Toast::SESSION_DELAY) }}"
              data-autohide="{{ session(\Orchid\Alert\Toast::SESSION_AUTO_HIDE) }}">
             <div class="toast-body p-3 bg-white rounded shadow-sm">
-                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" title="Close" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
                 <p class="mb-0">
                     <span class="text-{{ session(\Orchid\Alert\Toast::SESSION_LEVEL) }}">
                         <x-orchid-icon path="circle" class="mr-2"/>
@@ -35,6 +32,9 @@
 
                     {{ session(\Orchid\Alert\Toast::SESSION_MESSAGE) }}
                 </p>
+                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" title="Close" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
             </div>
         </div>
     @endif
